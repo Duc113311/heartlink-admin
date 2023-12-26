2261ecdda6d727d528b154749cf27bd26215f5a2
Duc Nguyen <ducnv@bachasoft.com>
2023-12-26 17:58:12 +0700

    sdwsds

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "dashboard-view",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
