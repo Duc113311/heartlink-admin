6de68c7f7c484c78ae5ebf81209c13e2f972721a
Duc Nguyen <ducnv@bachasoft.com>
2023-12-26 17:36:38 +0700

    puplic

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">
+      <div class="text-slate-50">About</div>
+    </router-link>
+  </nav>
+  <router-view /> -->
+
+  <div class="relative">
+    <div class="w-full h-full body-app">
+      <router-view />
+    </div>
+
+    <LoadApp
+      v-if="isShowLoading"
+      :urlImage="icUrlApp"
+      :codeColor="colorApp"
+      :bgColor="bgColorApp"
+    ></LoadApp>
+  </div>
+</template>
+
+<script>
+import LoadApp from "./layout/loading/load-app";
+export default {
+  components: { LoadApp },
+  name: "App",
+
+  data() {
+    return {
+      isShowLoading: false,
+      icUrlApp: require("@/assets/icon_svg/ic_logo.svg"),
+      colorApp: "#FF828A",
+      bgColorApp: "linear-gradient(#E1BA56,#F81C51)",
+    };
+  },
+
+  computed: {},
+
+  mounted() {},
+  methods: {},
+};
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/layout/loading/load-app.vue
@@ -0,0 +1,102 @@
+<template>
+  <div
+    class="absolute z-50 top-0 left-0 img-app items-center flex w-full h-full justify-center"
+  >
+    <div
+      class="loading-app absolute top-0 left-0 w-full h-full"
+      :style="`background: ${bgColorParam}`"
+    >
+      <div class="w-full h-full flex justify-center items-center">
+        <div
+          class="img-filter"
+          :style="`background-image: url('${urlImageParam}')`"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "load-app",
+  data() {
+    return {};
+  },
+
+  props: ["urlImage", "codeColor", "bgColor"],
+  computed: {
+    urlImageParam() {
+      return this.urlImage;
+    },
+
+    colorParam() {
+      const myStyle = {
+        backgroundColor: this.codeColor,
+      };
+      return myStyle;
+    },
+
+    bgColorParam() {
+      return this.bgColor;
+    },
+  },
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.loading-app {
+  z-index: 2100;
+}
+.img-filter {
+  width: 60px;
+  height: 60px;
+  background-position: revert;
+  background-size: cover;
+  position: absolute;
+  z-index: 9;
+}
+
+@keyframes pulseAnimate {
+  100% {
+    opacity: 0;
+    transform: scale(2);
+  }
+}
+
+.img-filter {
+  -webkit-animation: zoomeffect 5s infinite;
+  -moz-animation: zoomeffect 10s infinite;
+  animation: zoomeffect 5s infinite;
+}
+@-webkit-keyframes zoomeffect {
+  0% {
+    background-position: center;
+    transform: scale(1, 1);
+  }
+
+  50% {
+    background-position: center;
+    transform: scale(1.5, 1.5);
+  }
+  100% {
+    background-position: center;
+    transform: scale(1, 1);
+  }
+}
+@keyframes zoomeffect {
+  0% {
+    background-position: center;
+    transform: scale(1, 1);
+  }
+  50% {
+    background-position: center;
+    transform: scale(1.5, 1.5);
+  }
+  100% {
+    background-position: center;
+    transform: scale(1, 1);
+  }
+}
+</style>
