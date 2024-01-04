2b8c0f2bf018233592eeae9f86b9faeba6cc8073
Duc Nguyen <ducnv@bachasoft.com>
2024-01-04 18:09:50 +0700

    sdw

--- src/components/form/check-image.vue
+++ src/components/form/check-image.vue
@@ -100,7 +100,7 @@
                     id="checkbox-table-search-1"
                     type="checkbox"
                     v-model="selectedItems[index]"
-                    @change="handleCheckboxChange(item)"
+                    @change="handleCheckboxChange(item, index)"
                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
                   <label for="checkbox-table-search-1" class="sr-only"
